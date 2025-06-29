5985f2ac777b024a87cdfea3c2b5269d7f7bdf3c
Julian Gutierrez <157057593+JulianGut2@users.noreply.github.com>
2025-06-29 09:44:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ st.set_page_config(page_title = "CTA Ridership",layout = "centered")
 
 st.title("CTA Ridership Dashboard")
 
+df = pd.read.csv("../data/Cleaned_
+
 tab1, tab2, tab3, tab4 = st.tabs(['Overview', 'Data', 'Tab3', 'Tab4])
 
 with tab1:
     st.header("Project Overview")
     st.markdown("---")
     
+   
